89b232969244d02f63abbb6e0895ee9483bb87d5
Jason Alemán Torres <162923259+Jasongatobolador@users.noreply.github.com>
2026-04-18 20:15:53 -0600

    Update urls.py

--- convocatoria/urls.py
+++ convocatoria/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('Direcion-del-desarrollo-económico sostenible/', admin.site.urls),
     path('', include('convocatorias.urls')),
 ]
 
